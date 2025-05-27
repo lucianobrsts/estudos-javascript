5333761705234cbc81296123d42e1c3a3b68c1d5
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-05-26 23:12:09 -0300

    Variavéis

--- primeiro_projeto/js/script.js
+++ primeiro_projeto/js/script.js
@@ -1,1 +1,10 @@
 console.log('Javascript funcionando!');
+
+let firstName = 'Luciano';
+let lastName = 'Brito'
+
+console.log(firstName);
+console.log(lastName);
+
+let corCarro = 'vermelha';
+console.log(corCarro)
