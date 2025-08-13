a5bc0d734b711baa46fee4859fc8b268032b3769
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-08-12 23:42:11 -0300

    Trabalhando com objetos

--- primeiro_projeto/js/script.js
+++ primeiro_projeto/js/script.js
@@ -15,3 +15,21 @@ console.log(price);
 
 let myName = 'Luciano Brito';
 console.log(myName);
+
+let itemName = 'Pen'
+let itemPrice = 3
+let itemAvailable = true
+let itemcolor = 'Rede'
+
+console.log(itemName)
+
+let pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: 'Red'
+}
+
+pen.itemColor = 'Blue'
+
+console.log(pen)
