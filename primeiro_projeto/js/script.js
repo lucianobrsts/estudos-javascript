e5c26c5f5a39e72e34a5be29b3aea840c531a30c
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-08-24 09:23:38 -0300

    While loop

--- primeiro_projeto/js/script.js
+++ primeiro_projeto/js/script.js
@@ -180,3 +180,11 @@ for(i = 10; i <=20; i++) {
     console.log('Número: ' + i)
 }
 
+console.log('---------------------')
+console.log('While loop')
+var i = 1
+while(i <= 10) {
+    console.log('Número: ' + i)
+    i++
+}
+
