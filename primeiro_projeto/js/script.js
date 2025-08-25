f108be6b30af08706cb4ede8a47179271debf5b2
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-08-24 23:51:35 -0300

    for in loop

--- primeiro_projeto/js/script.js
+++ primeiro_projeto/js/script.js
@@ -194,3 +194,15 @@ do {
     console.log('Número: ', i)
     i++
 } while (i <= 10);
+
+console.log('---------------------')
+console.log('For in loop')
+const myCar = {
+    modelo: 'BMW',
+    year: 2023,
+    km: 68000
+}
+
+for(let i in myCar) {
+    console.log(i, myCar[i])
+}
