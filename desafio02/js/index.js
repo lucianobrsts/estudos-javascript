816cf161d5b81bf3d18bde793343ea563ef963b9
lucianobrsts <lucianobrsts@yahoo.com.br>
2026-01-24 11:33:38 -0300

    Finalizado desafio 02

--- desafio02/js/index.js
+++ desafio02/js/index.js
@@ -1,2 +1,7 @@
 let productValue = Number(prompt('Enter the product value: '))
-console.log(typeof(productValue))
+
+if(productValue >= 20) {
+    document.getElementById('result').innerHTML = 'Approved'
+} else {
+    document.getElementById('result').innerHTML = 'Denied'
+}20
