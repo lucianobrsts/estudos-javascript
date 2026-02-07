c480db899a7fe2b156885dc664e2d80ffca722a9
lucianobrsts <lucianobrsts@yahoo.com.br>
2026-02-07 12:16:48 -0300

    Ajustado e melhorado js para ter melhor desempenho e abranger mais funcoes

--- simple_calculator/js/script.js
+++ simple_calculator/js/script.js
@@ -1,18 +1,45 @@
 
-function outcome() {
-    let num1 = Number(document.getElementById('num1').value)
-    let num2 = Number(document.getElementById('num2').value)
-    let total = 0
+const num1Element = document.getElementById('num1')
+const num2Element = document.getElementById('num2')
+const totalElement = document.getElementById('total')
+const operationsRadios = document.getElementsByName('sign-area')
+const formElement = document.querySelector('form');
 
-    if(document.getElementById('radioAdd').checked) {
-        total = num1 + num2
-    } else if(document.getElementById('radioMinus').checked) {
-        total = num1 - num2
-    } else if(document.getElementById('radioMultiply').checked) {
-        total = num1 * num2
-    } else {
-        total = num1 / num2
+const operations = {
+    radioAdd: (a, b) => a + b,
+    radioMinus: (a, b) => a - b,
+    radioMultiply: (a, b) => a * b,
+    radioDivision: (a, b) => b !== 0 ? a / b : "Erro: div/0" 
+};
+
+function outcome() {
+    if(event) event.preventDefault();
+
+    const num1 = Number(num1Element.value)
+    const num2 = Number(num2Element.value)
+    
+    if(num1Element.value === '' || num2Element === '') {
+        totalElement.textContent = "Digite os números";
+        return;
     }
 
-    document.getElementById('total').innerHTML = String(total)
+    const selectedRadio = Array.from(operationsRadios).find(r => r.checked);
+      
+    let result;
+
+    if(selectedRadio && operations[selectedRadio.id]) {
+        result = operations[selectedRadio.id](num1, num2);
+    }
+
+    totalElement.textContent = typeof result === 'number' 
+    ? result.toLocaleString('pt-BR', { maximumFractionDigits: 4 })
+    : result;
 }
+
+function clear() {
+    formElement.reset();
+    totalElement.textContent = "0";
+}
+
+document.getElementById('btnCalculate').addEventListener('click', outcome)
+document.getElementById('btnClear').addEventListener('click', clear)
